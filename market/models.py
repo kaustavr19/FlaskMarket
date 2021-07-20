c208f2ef812fab5e73563e5790c26518828f1f56
Kaustav Roy <kaustavr25@gmail.com>
2021-07-21 00:46:54 +0530

    user auth and navbar edit

--- market/models.py
+++ market/models.py
@@ -7,13 +7,20 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
-    budgets = db.Column(db.Integer, nullable=False, default=1000)
+    budgets = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
 
+
+    @property
+    def prettier_budget(self):
+        if len(str(self.budgets)) >= 4:
+            return f'{str(self.budgets)[:-3]},{str(self.budgets)[-3:]}$'
+        else:
+            return f"{self.budgets}$"
 
     @property
     def password(self):
@@ -27,12 +34,12 @@ class User(db.Model, UserMixin):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
-    price = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
-    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
